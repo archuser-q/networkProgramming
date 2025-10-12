55dd2085bedcf1e9ed668ff62be1f0308d697058
archuser-q <availableemailaddress123@tutamail.com>
2025-10-12 21:01:40 +0700

    update

--- RMI/character.java
+++ RMI/character.java
@@ -1,36 +1,37 @@
 package RMI;
 
-import java.rmi.registry.*; 
+import java.rmi.registry.*;
 
 public class character {
     public static void main(String[] args){
         try {
             String studentCode = "B22DCVT419";
             String qCode = "NbXUKvt8";
-
+            
             Registry registry = LocateRegistry.getRegistry("203.162.10.109", 1099);
             CharacterService service = (CharacterService) registry.lookup("RMICharacterService");
 
-            String received = service.requestCharacter(studentCode, qCode); 
+            String received = service.requestCharacter(studentCode, qCode);
 
-            String[] processed = received.split(";");
-            String key = processed[0];
-            String unEncoded = processed[1];
+            String[] process = received.split(";");
 
-            String encoded = xor(unEncoded, key);
+            String key = process[0];
+            String input = process[1];
+
+            String encoded = key + xor(input, key);
 
             service.submitCharacter(studentCode, qCode, encoded);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public static String xor(String s, String key){
-        StringBuilder result = new StringBuilder();
-        for (int i=0; i<s.length(); i++){
-            char inputChar = s.charAt(i);
+    public static String xor(String input, String key){
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<input.length(); i++){
+            char inputChar = input.charAt(i);
             char keyChar = key.charAt(i%key.length());
-            result.append((char) (inputChar ^ keyChar));
+            sb.append((char)(inputChar ^ keyChar));
         }
-        return result.toString();
+        return sb.toString();
     }
 }
